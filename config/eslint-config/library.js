7430ba665420d934bc9b81729983983fadc37f9a
Marcelo Oliveira <marcelo.oliveira@dgtallab.com.br>
2025-08-02 19:10:36 -0300

    feat: create app web.

--- config/eslint-config/library.js
+++ config/eslint-config/library.js
@@ -1,8 +1,9 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['@rocketseat/eslint-config/react'],
-  plugins: ['simple-import-sort'],
+  plugins: ['simple-import-sort', 'unused-imports'],
   rules: {
     'simple-import-sort/imports': 'error',
+    'unused-imports/no-unused-imports': 'error',
   },
 }
